441cfdab289979dddeec4d77dce4fa55931e7260
Luis Garcia Figueres <garciafigueres@gmail.com>
2014-07-20 17:42:12 +0200

    Comentarios añadidos

--- src/com/garciafigueres/CalcularNif.java
+++ src/com/garciafigueres/CalcularNif.java
@@ -8,10 +8,6 @@
  * división entera del número del DNI entre 23. El resultado es un número comprendido entre el 0 y el 22. En base a una
  * tabla conocida se asigna una letra. La combinación del DNI con esa letra es el NIF.
  *
- * Este mismo algoritmo tambiénpuede utilizarse para el cálculo del NIE. En el caso que el NIE empiece por X se calcula
- * despreciando la X y utilizando los 7 dígitos, si el NIE empieza por Z, se sustituye la letra Z por el número 2 y se
- * realiza el mismo cálculo.
- *
  * TABLA DE ASIGNACIÓN
  * 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
  * T,R,W,A,G,M,Y,F,P,D,X,B,N,J,Z,S,Q,V,H,L,C,K,E
@@ -27,27 +23,41 @@ import java.util.Scanner;
 public class CalcularNif {
 
     public static void main(String[] args) {
+        //Creamos un objeto de la clase CalcularNif para acceder a los métodos de la misma
         CalcularNif obj = new CalcularNif();
+        //Creamos un objeto de la clase Scanner para recibir los datos introducidos por consola
         Scanner sc = new Scanner(System.in);
-        System.out.println("Por favor introduce el DNI: ");
+        System.out.print("Por favor introduce el DNI: ");
+        //Recibimos los datos por consola y los asignamos a una variable
         String dni = sc.next();
-        if (!obj.esEntero(dni)) {
-            System.out.println("El DNI introducido debe ser un número entero.");
+
+        //Llamamos al método que comprueba que la cadena introducida es un entero y positivo
+        if (!obj.esEnteroPositivo(dni)) {
+            //Si el método nos devuelve false (no es entero positivo) mostramos un mensaje
+            System.out.println("El DNI introducido debe ser un número entero positivo.");
         }else{
+            //Si el método nos devuelve true (es entero positivo) mostramos el NIF completo,
+            //llamando al método que calcula la letra del NIF, pasándole el dni como parámetro.
+            //Date cuenta de que al pasar el dni debemos CONVERTIRLO a int.
             System.out.println("NIF: " + dni + obj.calculaLetra(Integer.parseInt(dni)));
         }
     }
 
+    //Este método recibe un dni entero y positivo y calcula su letra correspondiente.
     public char calculaLetra(int dni){
         char letras[] = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
         int resto = dni%23;
         return letras[resto];
     }
 
-    public boolean esEntero(String cadena){
+    //Este método recibe una cadena y comprueba si puede convertirse a entero y positivo.
+    public boolean esEnteroPositivo(String cadena){
         try{
-            Integer.parseInt(cadena);
-            return true;
+            if(Integer.parseInt(cadena)>0) {
+                return true;
+            }else {
+                return false;
+            }
         }catch (Exception e) {
             return false;
         }
